b8b8bedc63eda7a0a01c838f18e1c29d2fe1f871
snoofoxes <christian.petallar17@gmail.com>
2024-06-18 20:59:33 -0700

    unang commit

---
+++ activity1.py
@@ -0,0 +1,21 @@
+"""
+print("'hello world'")
+print('"Hi, Im Christian Dan"')
+print("I'm 20 years old")
+
+for i in range(1,6):
+    i - 1
+    print(i)
+
+text = "Hello WOrld"
+print(text[0:-5])
+
+"""
+word = "summer bootcamp"
+
+print(word.title())
+print(f"{word[0:6].capitalize()}{word[6:14]}{word[-1].upper()}")
+print(f"L{word[-6]}{word[-6]}{word[-5]}")
+print(f"{word[11:14]}{word[4].upper()}")
+print(f"{word[12].upper()}{word[5].upper()}")
+print(word.replace(" ","a").isalpha())
